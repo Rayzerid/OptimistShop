7b928f5920fb0f3fcc606487388bd4d995df4831
Павел Гаврютченков <ugfof@mail.ru>
2024-06-05 13:40:00 +0300

    host

--- OptimistShop/Core/StoreDbContext.cs
+++ OptimistShop/Core/StoreDbContext.cs
@@ -7,9 +7,17 @@ namespace OptimistShop.Core
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //local
-            string hostname = System.Environment.GetEnvironmentVariable("COMPUTERNAME");
-            optionsBuilder.UseSqlServer(@$"Data Source={hostname}\SQLEXPRESS;Initial Catalog=optimist_store_ordering_system_db;Integrated security=True;TrustServerCertificate=True");
+            try
+            {
+                //host
+                optionsBuilder.UseSqlServer(@$"Server=217.28.223.127,17160;User Id=user_5d0ce;Password=Xy6=i$F92w?T;Database=db_e828a;TrustServerCertificate=True");
+            }
+            catch
+            {
+                //local
+                string hostname = System.Environment.GetEnvironmentVariable("COMPUTERNAME");
+                optionsBuilder.UseSqlServer(@$"Data Source={hostname}\SQLEXPRESS;Initial Catalog=optimist_store_ordering_system_db;Integrated security=True;TrustServerCertificate=True");
+            }
         }
         public DbSet<Clothes> Clothes { get; set; }
         public DbSet<ClothesContain> ClothesContain { get; set; }
